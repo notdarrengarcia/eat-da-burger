f1272c7bc2588e3dd48f5f2706228558ae997207
Darren Garcia <darrengarcia159@gmail.com>
2021-01-16 16:02:43 -0600

    initial push

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (NOT NULL,'Hamburger', FALSE, NOT NULL);
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (NOT NULL,'Bacon Jam Burger', FALSE, NOT NULL);
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (NOT NULL,'Poppy Burger', FALSE, NOT NULL);
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (NOT NULL,'Diablo Burger',FALSE, NOT NULL);
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (NOT NULL,'Smoked Burger',FALSE, NOT NULL);
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (NOT NULL,'Flower Burger',FALSE, NOT NULL);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,9 @@
+// packages being imported
+
+const express = require("express");
+const mysql = require("mysql");
+const exhbs = require("express-handlebars");
+
+
+
+
