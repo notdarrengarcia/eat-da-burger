c63679286f582b0d2007741a7fc2e3d69ec6975a
Darren Garcia <darrengarcia159@gmail.com>
2021-01-21 11:52:18 -0600

    updated handlebars

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-burger").on("click", function(event) {
+  $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var newDevour = 1;
 
-    var burgers = {
-      sleepy: newSleep
+    var newDevourState = {
+      devoured: 1
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newDevourState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("change devour to", newDevour);
         // Reload the page to get the updated list (possibly move it outside)
         location.reload();
       }
@@ -27,15 +27,17 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newBurger = $("#ca").val().trim();
-
+    var newBurger = { 
+      burger_name: $("#bu").val().trim(),
+      devoured: 0
+  };
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
       data: {burger_name: newBurger}
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
